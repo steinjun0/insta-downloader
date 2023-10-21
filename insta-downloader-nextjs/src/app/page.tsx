9a8af445b99f85a56f634c06605fe678540b7631
steinjun0 <1142308@naver.com>
2023-10-21 10:14:13 +0000

    update front

--- insta-downloader-nextjs/src/app/page.tsx
+++ insta-downloader-nextjs/src/app/page.tsx
@@ -1,103 +1,100 @@
 'use client';
-// import { scrap } from '@/service/scraping';
+
+import { Box, Button, Card, CardActions, CardContent, CircularProgress, Container, Grid, TextField, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useState } from 'react';
-import styles from './page.module.css';
-
-// const imgSrcs = scrap('https://www.instagram.com/p/CyA-20PrdS1/?img_index=1');
-// scrap('https://www.instagram.com/p/CyQ7_2RLUp4/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA%3D%3D')
-// async function getImages() {
-//   const imgSrcs = await scrap(
-//     'https://www.instagram.com/p/CyA-20PrdS1/?img_index=1'
-//   );
-//   return imgSrcs;
-// }
-
-// function getBlobUrls(urls: string[]): string[] {
-//   const blobUrls: string[] = [];
-
-//   urls.forEach(async (url: string) => {
-//     console.log('url', url);
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     const blob = await response.blob();
-
-//     // Blob URL을 생성합니다.
-//     const blobUrl = window.URL.createObjectURL(blob);
-//     blobUrls.push(blobUrl);
-
-//   });
-//   return blobUrls;
-
-// }
 
 export default function Home() {
-  // const imgSrcs = await getImages();
   const [imgSrcs, setImgSrcs] = useState<string[]>([]);
   const [url, setUrl] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Instagram Image Scraper</h1>
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="URL을 입력하세요."
-      />
-      <button
-        onClick={async () => {
-          const imgSrcs = await fetch(`/api/scrap?` + new URLSearchParams({ url: url }), { cache: 'force-cache' });
-          setImgSrcs(await imgSrcs.json());
-        }}
-      >
-        스크랩
-      </button>
-      {/* <button
-        onClick={async () => {
-          // const blogUrls = getBlobUrls(imgSrcs);
-          imgSrcs.forEach((url: string, index: number) => {
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = `image-${index}.png`; // 다운로드하는 파일의 이름을 설정합니다.
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-          }
-          );
-        }}
-      >전체 다운로드</button> */}
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', alignItems: 'center' }}>
-        {imgSrcs.map((src: string, i: number) => (
-          <div key={i} style={{ width: '300px', margin: '10px', position: 'relative' }}>
-            <Image
-              src={src}
-              alt="Picture of the author"
-              layout="responsive"
-              width={300} // 이 값은 실제 이미지의 비율에 따라 조정할 수 있습니다.
-              height={300} // 이 값은 실제 이미지의 비율에 따라 조정할 수 있습니다.
-              objectFit="contain" // 이미지가 컨테이너 안에 꽉 차도록 조정하되, 비율을 유지합니다.
+    <main>
+      <Container sx={{ display: 'flex', flexDirection: 'column', gap: '16px', paddingTop: '32px', paddingBottom: '32px' }}>
+        <h1>Instagram Image Downloader</h1>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '16px',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <TextField
+            fullWidth
+            label="URL"
+            variant="outlined"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="URL을 입력하세요."
+            disabled={isLoading}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ width: '100px', height: '56px' }}
+            onClick={async () => {
+              setImgSrcs([]);
+              setIsLoading(true);
+              const imgSrcs = await fetch(`/api/scrap?` + new URLSearchParams({ url: url }), { cache: 'force-cache' });
+              setImgSrcs(await imgSrcs.json());
+              setIsLoading(false);
+            }}
+            disabled={isLoading}
+          >
+            {isLoading ? <CircularProgress size={24} /> : '스크랩'}
+          </Button>
+        </Box>
 
-            />
-            <div style={{ position: 'absolute', right: 4, bottom: 10 }}>
-              <button
-                style={{ float: 'right', fontSize: '16px' }}
-                onClick={() => {
-                  const link = document.createElement('a');
-                  link.href = src;
-                  link.download = `image-${i}.png`; // 다운로드하는 파일의 이름을 설정합니다.
-                  document.body.appendChild(link);
-                  link.click();
-                  document.body.removeChild(link);
-                }}>
-                DOWN
-              </button>
-            </div>
 
-          </div>
-        ))}
-      </div>
+        <Grid container spacing={2} style={{ marginTop: '20px' }}>
+          {imgSrcs.length === 0 ? ( // imgSrcs 배열의 길이를 확인합니다.
+            !isLoading && ( // 로딩 중이 아니라면 메시지를 표시합니다.
+              <Typography variant="h6" style={{ margin: '0 auto' }}>
+                출력할 사진이 없습니다.
+              </Typography>
+            )
+          ) : (
+            imgSrcs.map((src: string, i: number) => (
+              <Grid item key={i} xs={12} sm={6} md={4}>
+                <Card
+                  variant='outlined'
+                >
+                  <CardContent>
+                    <Image
+                      src={src}
+                      alt="Scraped from Instagram"
+                      layout="responsive"
+                      width={300}
+                      height={300}
+                      objectFit="contain"
+                    />
+                  </CardContent>
+                  <CardActions sx={{ justifyContent: 'flex-end' }}>
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => {
+                        const link = document.createElement('a');
+                        link.href = src;
+                        link.download = `image-${i}.png`;
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                      }}
+                    >
+                      Download
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
+
     </main>
   );
 }
